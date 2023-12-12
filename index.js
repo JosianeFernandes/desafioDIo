3c06b40bf308a64926103048e3de9f8b9f329bdc
JosianeFernandes <107226209+JosianeFernandes@users.noreply.github.com>
2023-12-12 09:38:18 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log ("ola")
+let jogador = "Felipe"
+//let ponto = 0
+
+for ( let ponto = 0; ponto <= 1000 ; ponto++) {
+console.log (ponto)
+let  nivel="Ferro"
+console.log ("O Herói de nome " + jogador +  " está no nível de " + nivel)}
+
+for ( let ponto = 1000; ponto <= 2000 ; ponto++) {
+    console.log (ponto)
+    let  nivel="Bronze"
+    console.log ("O Herói de nome " + jogador +  " está no nível de " + nivel)}
+    
+    for ( let ponto = 2001; ponto <= 5000 ; ponto++) {
+        console.log (ponto)
+        let  nivel="Prata"
+        console.log ("O Herói de nome " + jogador +  " está no nível de " + nivel)}
+
+        for ( let ponto = 6000; ponto <= 7000 ; ponto++) {
+            console.log (ponto)
+            let  nivel="Ouro"
+            console.log ("O Herói de nome " + jogador +  " está no nível de " + nivel)}
+    
+            for ( let ponto = 7000; ponto <= 8000 ; ponto++) {
+                console.log (ponto)
+                let  nivel="Platina"
+                console.log ("O Herói de nome " + jogador +  " está no nível de " + nivel)}
+
+                for ( let ponto = 8000; ponto <= 9000 ; ponto++) {
+                    console.log (ponto)
+                    let  nivel="Ascendente"
+                    console.log ("O Herói de nome " + jogador +  " está no nível de " + nivel)}
+                  
+                    for ( let ponto = 9000; ponto <= 10000 ; ponto++) {
+                        console.log (ponto)
+                        let  nivel="Imortal"
+                        console.log ("O Herói de nome " + jogador +  " está no nível de " + nivel)}
+                      
+                        for ( let ponto = 10000; ponto <= 11000 ; ponto++) {
+                            console.log (ponto)
+                            let  nivel="Imortal"
+                            console.log ("O Herói de nome " + jogador +  " está no nível de " + nivel)}
+                                     
+
+
+//Se XP for maior ou igual a 10.001 = Radiante)
+
